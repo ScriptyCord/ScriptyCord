033d43e73d9797b29b9558a952af468309204651
TheSlipper <korneldomeradzki1@gmail.com>
2022-08-09 23:32:57 +0200

    startup setup

---
+++ ScriptCord.Bot/Commands/TestingModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCord.Bot.Commands
+{
+    public class TestingModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        public Task PingAsync()
+            => ReplyAsync("Pong!");
+    }
+}
--- ScriptCord.Bot/Program.cs
+++ ScriptCord.Bot/Program.cs
@@ -1,9 +1,52 @@
-﻿namespace ScriptCord.Bot
+﻿using Autofac;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using ScriptCord.Bot.Commands;
+
+namespace ScriptCord.Bot
 { 
     class Bot
     {
         public static void Main(string[] args)
+            => new Bot().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
+            using (var container = SetupAutofac())
+            {
+                var scope = container.BeginLifetimeScope();
+
+                var client = scope.Resolve<DiscordSocketClient>();
+                client.Log += LogAsync;
+
+                // TODO: This below could go into the constructor perhaps once the logging is moved to a class
+                scope.Resolve<CommandService>().Log += LogAsync;
+
+                //await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
+                await client.LoginAsync(TokenType.Bot, "NDgxODkyNjIzMDgzMzcyNTQ0.GdnlGB.RzfEFT8pFFMbAz7E95NLpANo4ZtYrtSNKUKWRk");
+                await Task.Delay(Timeout.Infinite);
+            }
+        }
+
+        public IContainer SetupAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DiscordSocketClient>().As<DiscordSocketClient>();
+            builder.RegisterType<CommandService>().As<CommandService>();
+            builder.RegisterType<HttpClient>().As<HttpClient>();
+
+            builder.RegisterType<TestingModule>().As<TestingModule>();
+            builder.RegisterType<CommandHandlingService>().As<CommandHandlingService>();
+            return builder.Build();
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            // TODO: normal logger for this
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
         }
     }
 }
