a7eaba268b4154da1cd59f38cc896dd14fd57413
TheSlipper <korneldomeradzki1@gmail.com>
2022-09-28 18:37:23 +0200

    fixed pattern url matching to include shorts - resolve #13

--- ScriptyCord.Bot/Services/Playback/PlaylistEntriesService.cs
+++ ScriptyCord.Bot/Services/Playback/PlaylistEntriesService.cs
@@ -169,7 +169,7 @@ namespace ScriptCord.Bot.Services.Playback
         private Result<IAudioManagementStrategy> GetSuitableStrategyByUrl(string url)
         {
             IAudioManagementStrategy audioManagementStrategy = null;
-            if (url.StartsWith("https://www.youtube.com/watch") || url.StartsWith("https://youtu.be/") || url.StartsWith("https://m.youtube.com/"))
+            if (url.StartsWith("https://www.youtube.com/") || url.StartsWith("https://youtu.be/") || url.StartsWith("https://m.youtube.com/"))
                 audioManagementStrategy = new YouTubeAudioManagementStrategy(_configuration);
             else
             {
